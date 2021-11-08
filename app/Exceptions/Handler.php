e49c4340a93050f91bc7a2a192393f486bdd73e3
David Peach <mail@davidpeach.co.uk>
2021-11-08 09:59:24 +0000

    Add Laravel coding standards and get to initial green.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,6 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Throwable;
 
 class Handler extends ExceptionHandler
 {
@@ -13,7 +12,6 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontReport = [
-        //
     ];
 
     /**
@@ -29,13 +27,8 @@ class Handler extends ExceptionHandler
 
     /**
      * Register the exception handling callbacks for the application.
-     *
-     * @return void
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
     }
 }
