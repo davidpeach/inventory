f976693507ce9dc4263930f7248d3e6854ecbde8
David Peach <mail@davidpeach.co.uk>
2023-01-18 18:12:52 +0000

    Add docblocks for phpstan to understand code.

--- app/Http/Resources/InventoryResource.php
+++ app/Http/Resources/InventoryResource.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Inventory;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @mixin Inventory
+ */
 class InventoryResource extends JsonResource
 {
+    /**
+     * @return array<string, string>
+     */ 
     public function toArray($request): array
     {
         return [
